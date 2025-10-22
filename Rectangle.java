db06734101d7d9a687cc9dd10bc411021d05ff6c
Shaun <shaun.danny@jnis.ac.in>
2025-10-22 17:19:33 -0400

    feat: add Circle class for shape implementation

---
+++ Rectangle.java
@@ -0,0 +1,34 @@
+package lab4;
+
+public class Rectangle {
+    Point A;
+    Point B;
+    Point C;
+    Point D;
+
+    public Rectangle(Point A, Point B, Point C, Point D) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+    }
+
+    public double perimeter() {
+        return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A);
+    }
+
+    public boolean isSquare() {
+        double dAB = A.distance(B);
+        double dBC = B.distance(C);
+        double dCD = C.distance(D);
+        double dDA = D.distance(A);
+
+        double diagAC = A.distance(C);
+        double diagBD = B.distance(D);
+
+        if (dAB == dBC && dBC == dCD && dCD == dDA && diagAC == diagBD)
+            return true;
+        else
+            return false;
+    }
+}
