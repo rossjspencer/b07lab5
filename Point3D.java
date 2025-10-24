2c7d7387c15b3a79ed7353f257d0aac173d2933a
Ross Spencer <ross.j.spencer@gmail.com>
2025-10-24 18:40:00 -0400

    feat: added Point3D class to define icosahedron vertices

---
+++ Point3D.java
@@ -0,0 +1,87 @@
+package lab4;
+
+public class Point3D extends Point
+{
+    double x;
+    double y;
+    double z;
+
+    public Point3D(double x, double y, double z)
+    {
+        super(x, y);
+        this.z = z;
+    }
+
+    @Override
+    public double distance(Point other)
+    {
+        if (!(other instanceof Point3D))
+        {
+            throw new IllegalArgumentException("Distance requires another 3D point.");
+        }
+
+        Point3D p = (Point3D) other;
+        return Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2) + Math.pow(z - p.z, 2));
+    }
+
+    @Override
+    public double findInteriorAngle(Point B, Point C) throws Exception
+    {
+        if (!(B instanceof Point3D) || !(C instanceof Point3D))
+        {
+            throw new Exception("Arguments must be Point3D.");
+        }
+
+        Point3D b = (Point3D) B;
+        Point3D c = (Point3D) C;
+
+        double vABX = b.x - this.x;
+        double vABY = b.y - this.y;
+        double vABZ = b.z - this.z;
+        double magAB = Math.sqrt(vABX * vABX + vABY * vABY + vABZ * vABZ);
+
+        double vBCX = c.x - b.x;
+        double vBCY = c.y - b.y;
+        double vBCZ = c.z - b.z;
+        double magBC = Math.sqrt(vBCX * vBCX + vBCY * vBCY + vBCZ * vBCZ);
+
+        if (magAB == 0 || magBC == 0)
+        {
+            throw new Exception("Vector magnitudes cannot be zero.");
+        }
+
+        double dotProduct = vABX * vBCX + vABY * vBCY + vABZ * vBCZ;
+        double angle = Math.acos(dotProduct / (magAB * magBC));
+
+        //just in case
+        if (angle > Math.PI)
+        {
+            angle = 2 * Math.PI - angle;
+        }
+
+        return angle;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return (int)(3*x + 5*y + 7*z);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+
+        if (!(obj instanceof Point3D))
+        {
+            return false;
+        }
+
+        Point3D other = (Point3D) obj;
+        return x == other.x && y == other.y && z == other.z;
+    }
+}
